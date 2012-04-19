ed20c15144649fb30e4dd14e5462d85e9858db2c
Ryan Clark <ryan.clark.j@gmail.com>
2012-04-19 14:45:31 -0700

    Personalized for azphysiobiotic map

--- js/Main.js
+++ js/Main.js
@@ -2,32 +2,73 @@ function init(){
 	var map = new L.Map("map");
 	
 	/* Tilestream Layer example: */
-	var historicUrl = "http://opengis.azexperience.org/tiles/v2/azHistoric1880/{z}/{x}/{y}.png",
-		historicLayer = new L.TileLayer(historicUrl, {maxZoom: 10}); 
+	var bioticUrl = "http://opengis.azexperience.org/tiles/v2/azphysiobiotic/{z}/{x}/{y}.png",
+		bioticLayer = new L.TileLayer(bioticUrl, {maxZoom: 12, opacity: 0.6}); 
 	
 	/* ESRI tiled service example: */
-	var natGeoLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer");
-	
-	/* Bing maps example: 
-	var bingLayer = new L.TileLayer.Bing(<<Bing Maps API Key>>, "Road"); */
-	
-	/* WMS layer example: */
-	var wmsUrl = "http://opengis.azexperience.org/geoserver/wms",
-		wmsLayer = new L.TileLayer.WMS(wmsUrl, { 
-			maxZoom: 10, 
-			layers: "vae:azhistoricmines", 
-			format: "image/png", 
-			transparent: true 
-		}); 
+	var imageryLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer");
+	var refLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer");
 	
 	/* WFS GeoJSON layer example: */
-	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:azhistoricmines", {
-		pointToLayer: function(latlng) { return new L.CircleMarker(latlng); },
-		popupObj: new JadeContent("templates/example.jade"),
-		popupOptions: { maxWidth: 530, centered: false },
+	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:azpointsofinterest", {
+		pointToLayer: function(latlng) { 
+			return new L.Marker(latlng, {
+				icon: new L.Icon({
+					iconUrl: "style/images/fancy-binoculars.png",
+					iconSize: new L.Point(40, 40)
+				})				
+			}); 
+		},
+		popupObj: new JadeContent("templates/pointofinterest.jade"),
+		popupOptions: { maxWidth: 530, centered: true },
+		popupFn: function(e) {
+			var map = e.layer._map;
+			if (map.activePanel && map.activePanel.hidden == false) { map.activePanel.hide(); }
+		},
 		hoverFld: "name"
-	}); 
+	});
+	
+	var legendPanel = new L.Control.Panel({
+		id: "legend-panel",
+		content: "Hello there",
+		width: 400,
+		template: new JadeContent("templates/polys.jade"),
+		onShow: function() {
+			$("#chooser div").click(function() {				
+				var id = $(this).attr("id");
+				var bio = $("#biotic-info"), phys = $("#physio-info");
+				if (id == "bio") {
+					$("#bio").addClass("chosen-chooser");
+					$("#phys").removeClass("chosen-chooser");
+					bio.removeClass("hidden-content");
+					phys.addClass("hidden-content");
+				} else {
+					$("#phys").addClass("chosen-chooser");
+					$("#bio").removeClass("chosen-chooser");
+					bio.addClass("hidden-content");
+					phys.removeClass("hidden-content");
+				}
+			});
+		}
+	});
+	
+	map.addControl(legendPanel);		
+	
+	map.clickResponse = new L.GeoJSON.WFS.ClickResponder({
+		map: map,
+		url: "http://opengis.azexperience.org/geoserver/wfs",
+		featureType: "vae:azphysiobioticpolys",
+		version: "1.0.0",
+		geomFieldName: "wkb_geometry",
+		panelObj: legendPanel
+	});
 	
 	var center = new L.LatLng(34.1618, -111.53332);
-	map.setView(center, 7).addLayer(natGeoLayer);
+	map.setView(center, 7).addLayer(imageryLayer).addLayer(wfsLayer);
+	setTimeout(function() {
+		map.addLayer(bioticLayer);
+		setTimeout(function() {
+			map.addLayer(refLayer);			
+		}, 250);
+	}, 250);
 }
